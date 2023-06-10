2e6912e1672318e6df23cd41565cdeaef1ab83c0
Robson Amorim <92536182+robsondmtt@users.noreply.github.com>
2023-06-09 23:19:16 -0300

    inicial

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const nomes = ['João', 'Maria', 'Pedro', 'Ana', 'Carlos']; // Array de nomes pré-definidos
+  const [sorteado, setSorteado] = useState(null); // Nome sorteado
+  const [mudando, setMudando] = useState(false); // Flag para controlar a mudança rápida dos nomes
+
+  // Função para sortear um nome aleatório do array
+  const sortearNome = () => {
+    const indiceSorteado = Math.floor(Math.random() * nomes.length);
+    setSorteado(nomes[indiceSorteado]);
+    setMudando(false);
+  };
+
+  // UseEffect para controlar a mudança rápida dos nomes
+  useEffect(() => {
+    let intervalId;
+
+    if (mudando) {
+      intervalId = setInterval(sortearNome, 100);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [mudando]);
+
+  return (
+    <div className="App">
+      <h1>Sorteio de Nomes</h1>
+      <div className="nomes-container">{mudando ? 'Sorteando...' : sorteado}</div>
+      <button onClick={() => setMudando(!mudando)}>
+        {mudando ? 'Parar' : 'Sortear'}
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
