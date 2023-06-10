f0a08f33ec4d0783c6b88c5a267a780a436b4fe4
Robson Amorim <92536182+robsondmtt@users.noreply.github.com>
2023-06-10 13:30:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const nomes = ['João', 'Maria', 'Pedro', 'Ana', 'Carlos']; // Array de nomes pré-definidos
+  const nomes = ['Jerlis', 'Beatriz_ll', 'Carmosadasilva', 'Suzieandre', 'Madalena','Luana.sousa.98229',
+                 'gleycehellensantoscostas','Keillagomessilva','Cassiane_lima_2310','Tatianesilvanava2',
+                 '_cleisasiqueira','eligolveia','sandrafontinele2','miriamnandes','taynarabarbosa60','kelynavalima',
+                 'santos_kelly56','biamattosofficial','rosangelasilva5766','gi_nino','mixstoreoficial','limap4419',
+                 'polinne14','juniior_silvah7','genilda_silva88','regi.na983','vanda_rb.gomes','noeliasilva02kk',
+                 'sarah.menezws52','alinnemarquess','hiedanascimentonascimento','silvana2015coimbra'
+                ]; // Array de nomes pré-definidos
   const [nomeAtual, setNomeAtual] = useState(''); // Nome atual que está sendo exibido
   const [mudando, setMudando] = useState(true); // Flag para controlar a mudança rápida dos nomes
 
