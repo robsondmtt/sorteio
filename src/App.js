9a053fc9fb95d23fc129bee2042dde00ce0f2ea7
Robson Amorim <92536182+robsondmtt@users.noreply.github.com>
2023-06-10 15:32:58 -0300

    app pronto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import { Box, Button, Center, Heading, Image, Text } from '@chakra-ui/react';
-import './App.css';
 import { useEffect, useState } from 'react';
 import logo from './assets/logo1.png';
+import nomes from './dadosSorteio';
 
 function App() {
 
-  //NOMES DOS PARTICIPANTES DO SORTEIO
-  const nomes = ['Jerlis', 'Beatriz_ll', 'Carmosadasilva', 'Suzieandre', 'Madalena', 'Luana.sousa.98229',
-    'gleycehellensantoscostas', 'Keillagomessilva', 'Cassiane_lima_2310', 'Tatianesilvanava2',
-    '_cleisasiqueira', 'eligolveia', 'sandrafontinele2', 'miriamnandes', 'taynarabarbosa60', 'kelynavalima',
-    'santos_kelly56', 'biamattosofficial', 'rosangelasilva5766', 'gi_nino', 'mixstoreoficial', 'limap4419',
-    'polinne14', 'juniior_silvah7', 'genilda_silva88', 'regi.na983', 'vanda_rb.gomes', 'noeliasilva02kk',
-    'sarah.menezws52', 'alinnemarquess', 'hiedanascimentonascimento', 'silvana2015coimbra'
-  ];
 
   const [nomeAtual, setNomeAtual] = useState(''); // Nome atual que está sendo exibido
   const [mudando, setMudando] = useState(true); // Flag para controlar a mudança rápida dos nomes
 
-  // UseEffect para controlar a mudança rápida dos nomes
   useEffect(() => {
     let intervalId;
 
@@ -39,7 +30,6 @@ function App() {
   };
 
   return (
-    // <div className="App" style={{background:'pink', }}>
     <Center bg={'#FEF1F9'}>
       <Box h={'100vh'} mt={'15vh'} bg='#FEF1F9'>
         <Center><Image src={logo} w='250px' alt={logo} /></Center>
@@ -57,8 +47,8 @@ function App() {
             </Button>
           )}
         </Center>
-      </Box></Center>
-    // </div>
+      </Box>
+    </Center>
   );
 }
 
