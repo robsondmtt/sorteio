e0ee0614e48363834ef3fd6607f37f5f2bcdbf02
Robson Amorim <92536182+robsondmtt@users.noreply.github.com>
2023-06-10 20:07:48 -0300

    organizacao

--- src/dadosSorteio.js
+++ src/dadosSorteio.js
@@ -1,9 +1,36 @@
-const nomes = ['Jerlis', 'Beatriz_ll', 'Carmosadasilva', 'Suzieandre', 'Madalena', 'Luana.sousa.98229',
-    'gleycehellensantoscostas', 'Keillagomessilva', 'Cassiane_lima_2310', 'Tatianesilvanava2',
-    '_cleisasiqueira', 'eligolveia', 'sandrafontinele2', 'miriamnandes', 'taynarabarbosa60', 'kelynavalima',
-    'santos_kelly56', 'biamattosofficial', 'rosangelasilva5766', 'gi_nino', 'mixstoreoficial', 'limap4419',
-    'polinne14', 'juniior_silvah7', 'genilda_silva88', 'regi.na983', 'vanda_rb.gomes', 'noeliasilva02kk',
-    'sarah.menezws52', 'alinnemarquess', 'hiedanascimentonascimento', 'silvana2015coimbra'
-  ];
+const nomes = [
+  'Jerlis',
+  'Beatriz_ll',
+  'Carmosadasilva',
+  'Suzieandre',
+  'Madalena',
+  'Luana.sousa.98229',
+  'gleycehellensantoscostas',
+  'Keillagomessilva',
+  'Cassiane_lima_2310',
+  'Tatianesilvanava2',
+  '_cleisasiqueira',
+  'eligolveia',
+  'sandrafontinele2',
+  'miriamnandes',
+  'taynarabarbosa60',
+  'kelynavalima',
+  'santos_kelly56',
+  'biamattosofficial',
+  'rosangelasilva5766',
+  'gi_nino',
+  'mixstoreoficial',
+  'limap4419',
+  'polinne14',
+  'juniior_silvah7',
+  'genilda_silva88',
+  'regi.na983',
+  'vanda_rb.gomes',
+  'noeliasilva02kk',
+  'sarah.menezws52',
+  'alinnemarquess',
+  'hiedanascimentonascimento',
+  'silvana2015coimbra'
+];
 
-  export default nomes;
+export default nomes;
